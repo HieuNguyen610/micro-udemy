d26c4ab5528eb96ee87299f87ba3c5a2d8d1294f
hieunm2 <hieunm2@vnext.vn>
2025-03-18 13:50:39 +0700

    int commit

---
+++ src/main/java/hieu/microudemy/MicroUdemyApplication.java
@@ -0,0 +1,13 @@
+package hieu.microudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hieu/microudemy/MicroUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package hieu.microudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

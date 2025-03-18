6955ac9da7182022a3ec254b3eae10ec9adafaad
hieunm2 <hieunm2@vnext.vn>
2025-03-18 17:27:06 +0700

    statistic module

---
+++ statisticservice/src/main/java/hieu/statisticservice/enitity/Statistic.java
@@ -0,0 +1,25 @@
+package hieu.statisticservice.enitity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "statistic")
+public class Statistic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_date")
+    private Date createdDate;
+}
